72846be4aaabc0c5d68d4c49e38046adc5197bba
Sutangu <sutangu@gmail.com>
2018-07-25 22:54:34 +0600

    Edit Profile Component

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -18,6 +18,9 @@ class Navbar extends Component {
         const authLinks = (
             <ul className="navbar-nav ml-auto">
                 <li className="nav-item">
+                    <Link className="nav-link" to="/dashboard">Dashboard</Link>
+                </li>
+                <li className="nav-item">
                     <a href="" onClick={this.onLogoutClick} className="nav-link">
                         <img className="rounded-circle" src={user.avatar} alt={user.name} style={{ width: '25px', marginRight: '5px' }} title="You must have a Gravatar connected to your email to dispalay an image" />
                         {' '} Logout
