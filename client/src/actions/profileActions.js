b29f62d6075055d9a6bb0b07cd18cf45d9ce7065
Sutangu <sutangu@gmail.com>
2018-07-26 21:22:32 +0600

    Profiles Component & getProfiles Action

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS, SET_CURRENT_USER } from './types';
+import { GET_PROFILE, GET_PROFILES, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS, SET_CURRENT_USER } from './types';
 
 // Get current profile
 export const getCurrentProfile = () => dispatch => {
@@ -90,7 +90,25 @@ export const deleteEducation = id => dispatch => {
                 payload: err.response.data
             })
         );
-}
+};
+
+// Get all profiles
+export const getProfiles = () => dispatch => {
+    dispatch(setProfileLoading());
+    axios
+        .get(`/api/profile/all`)
+        .then(res =>
+            dispatch({
+                type: GET_PROFILES,
+                payload: res.data
+            }))
+        .catch(err =>
+            dispatch({
+                type: GET_PROFILES,
+                payload: null
+            })
+        );
+};
 
 // Delete account & profile
 export const deleteAccount = () => dispatch => {
