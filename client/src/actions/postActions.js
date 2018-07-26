0f40c287fa8b5280abb47ef653ad20b35c3457e1
Sutangu <sutangu@gmail.com>
2018-07-27 02:51:47 +0600

    getPosts Action & PostFeed Component

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 import {
     ADD_POST,
-    GET_ERRORS
+    GET_ERRORS,
+    GET_POSTS,
+    POST_LOADING
 } from './types';
 
 // Add Post
@@ -22,3 +24,29 @@ export const addPost = postData => dispatch => {
             })
         );
 }
+
+// Get Posts
+export const getPosts = () => dispatch => {
+    dispatch(setPostLoading());
+    axios
+        .get('/api/posts')
+        .then(res =>
+            dispatch({
+                type: GET_POSTS,
+                payload: res.data
+            })
+        )
+        .catch(err =>
+            dispatch({
+                type: GET_POSTS,
+                payload: null
+            })
+        );
+}
+
+// Set loading state
+export const setPostLoading = () => {
+    return {
+        type: POST_LOADING
+    }
+};
