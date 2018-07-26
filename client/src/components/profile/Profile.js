687298cbda744f61b4cac3d6af08b83ace559bb7
Sutangu <sutangu@gmail.com>
2018-07-27 00:04:39 +0600

    Profile Header

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -17,16 +17,43 @@ class Profile extends Component {
     }
 
     render() {
+        const { profile, loading } = this.props.profile;
+        let profileContent;
+
+        if (profile === null || loading) {
+            profileContent = <Spinner />;
+        } else {
+            profileContent = (
+                <div>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <Link to="/profiles" className="btn btn-light mb-3 float-left">
+                                Back To Profiles
+                            </Link>
+                        </div>
+                        <div className="col-md-6" />
+                    </div>
+                    <ProfileHeader profile={profile} />
+                    <ProfileAbout />
+                    <ProfileCreds />
+                    <ProfileGithub />
+                </div>
+            );
+        }
+
         return (
-            <div>
-                <ProfileHeader />
-                <ProfileAbout />
-                <ProfileCreds />
-                <ProfileGithub />
-            </div>
-        )
-    } 
-}
+                    <div>
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-md-12">
+                                    {profileContent}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }       
+        }
 
 Profile.propTypes = {
     getProfileByHandle: PropTypes.func.isRequired,
@@ -34,7 +61,7 @@ Profile.propTypes = {
 }
 
 const mapStateToProps = state => ({
-    profile: state.profile  
+    profile: state.profile              
 });
 
 export default connect(mapStateToProps, {getProfileByHandle})(Profile);
