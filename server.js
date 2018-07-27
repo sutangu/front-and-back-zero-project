b0cb9cf3d5dd4a213570cd5f21984df2fc2147a1
Enclave88 <sutangu@gmail.com>
2018-07-27 12:25:35 +0600

    Prepared for deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -18,9 +19,9 @@ const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
-  .connect(db)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
+    .connect(db)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error(err));
 
 // Passport middleware
 app.use(passport.initialize());
@@ -32,6 +33,16 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
+
+// Server static assets if in producrion
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const port = process.env.PORT || 5000;
 
